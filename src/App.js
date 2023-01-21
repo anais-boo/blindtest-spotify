eb31f805edfacf2be55e80e35387245c5171ca12
Anaïs Boo <anaisb@chalmers.se>
2023-01-21 19:07:35 +0100

    adding text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <header className={styles.appHeader}>
         <img src={logo} className={styles.appLogo} alt="logo" />
         <h1 className={styles.appTitle}>Bienvenue sur le Blindtest</h1>
-        <p>Il va falloir modifier le code pour faire un vrai Blindtest !</p>
+        <p>Il va falloir modifier le code pour faire un vrai Blindtest ! Allons-y!!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
